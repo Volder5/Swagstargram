90ce5cd2d52e320fc355c518d7fc8ae8cc2e547b
Vova Prykhodki <vovaprykhodki@Vovas-MacBook-Air.local>
2025-05-13 18:35:20 +0200

    Security update...

--- config/customtokens.py
+++
@@ -1,4 +0,0 @@
-EMAIL_HOST_USER = 'noreply.swagstargram@gmail.com'
-EMAIL_HOST_PASSWORD = 'vogv wuzn mqnt cneh'
-DEFAULT_FROM_EMAIL = 'noreply.swagstargram@gmail.com'
-SECRET_KEY = 'django-insecure-czq#q($6j!v^w45aof(u9)i77frmrx26l(56s17a*2cz&%)wg0'
